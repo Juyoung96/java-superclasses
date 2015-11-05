dfe0df5e8d82f39dff09c70af04c6175ce3a75e7
Juyoung96 <leejuyoung@gpa.ac.kr>
2015-11-05 13:56:55 +0900

    Abstract

--- Tester.java
+++ Tester.java
@@ -22,12 +22,27 @@ public class Tester {
     // Test the shapes
     Circle c = new Circle();
     Cylinder cy = new Cylinder( );
-    
+ 
     System.out.println( "The area of the first circle is: " + c.findArea() );
     System.out.println( "The area of the cylinder circle is " + cy.findArea() );
     System.out.println( "The volume of the cylnder is: " + cy.findVolume() );
 
+    //Test the Abstract class
+    System.out.println();
     
+    Square sq = new Square();
+    System.out.println( "Square area is: " + sq.findArea( sq.getLength(), sq.getWidth() ) );
+    
+    // Test the Interface
+    System.out.println();
+    
+    Shark shark = new Shark();
+    shark.hunt();
+    shark.eat();
+    shark.swim();
+    shark.run();
+    shark.jump();
+    shark.hide();
     
   } // END main method
   
